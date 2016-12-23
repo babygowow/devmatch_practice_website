e0233f56fc28317a7f5705630bc6a7a7307a4088
Alex Draper <babygowow@gmail.com>
2016-12-23 02:18:14 +0000

    Added bootstrap gem to website.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-    def home #This method is the method that controls the home page of website.
+    def home #This method is the method that "controls" the home page of website.
     end
     
     def about #About page.
